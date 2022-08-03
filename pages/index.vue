21c51a012d88dfe1fabc3d5a325d91c3a88a9986
Eric-Weng <weng.eric8@gmail.com>
2022-08-03 12:36:35 -0600

    feat: tailwind, typography, and daisyui, font-awesome, content, vue-scrollto installed

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <Tutorial/>
+  <div class="text-4xl">
+    <font-awesome-icon :icon="['fab', 'github']"/>
+    <NuxtLink to="/guides/test">link</NuxtLink>
+  </div>
 </template>
 
 <script>
