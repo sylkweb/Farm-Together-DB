753696b2657cfb1a3601a60e16044a9024ab79cf
Eric-Weng <weng.eric8@gmail.com>
2022-08-20 17:50:44 -0600

    feat: Added non 404 error page.

--- layouts/error.vue
+++ layouts/error.vue
@@ -24,6 +24,22 @@ export default {
         </NuxtLink>
       </div>
     </div>
-    <div v-else></div>
+    <div v-else>
+      <div class="flex flex-col justify-center align-center text-center">
+        <h1 class="mb-2 font-bold text-3xl text-primary md:text-8xl">Oops!</h1>
+        <h2 class="font-medium text-xl md:text-3xl">
+          Something went wrong! This seems to be the line for the bathroom. Not much farming to do here...
+        </h2>
+        <img
+          class="max-w-screen mx-auto my-10"
+          src="https://cdn.akamai.steamstatic.com/steamcommunity/public/images/clans/32023457/614b353ac7ecc5354eeddea30be7548987106918.gif"
+          alt="404 chickens"
+        />
+        <h3 class="text-lg md:text-2xl">Back to the lettuce fields!</h3>
+        <NuxtLink class="my-6" to="/">
+            <button class="btn btn-lg btn-outline btn-primary">Head Home!</button>
+        </NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
