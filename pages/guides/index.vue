dc91c7b5137349728d7c6e926240b0dd357a1e5c
Eric Weng <37000771+Eric-Weng@users.noreply.github.com>
2022-09-11 00:09:32 -0600

    Merge branch 'develop' of https://github.com/sylkweb/farm-together-db into feat/create-guide-landing-template
    # Conflicts:
    #	pages/guides/index.vue

--- pages/guides/index.vue
+++ pages/guides/index.vue
@@ -5,5 +5,41 @@ export default {
 </script>
 
 <template>
-  <div></div>
+  <div>
+    <!-- Title and Description -->
+    <div class="my-6">
+      <h1 class="mx-auto text-center text-8xl font-semibold">Guides</h1>
+      <h2 class="mx-auto mt-4 text-center text-lg">
+        A collection of Guides from the developers of Farm Together DB
+      </h2>
+    </div>
+
+    <!-- Guide Category -->
+    <div>
+      <h2>Section Title</h2>
+      <div class="grid grid-cols-3 gap-4">
+        <div class="justify-between h-32 bg-base-100 border-secondary-focus border-2 rounded-md transition-all hover:drop-shadow-lg hover:-translate-y-1">
+          <div class="flex flex-row w-full h-full">
+            <!-- Article Info -->
+            <div class="flex flex-col">
+              <h3 class="font-semibold text-primary text-xl">
+                This 
+              </h3>
+              <p>This is</p>
+            </div>
+            <!-- Article Image -->
+            <div class="my-auto mx-auto">
+              <div class="avatar">
+                <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                  <img src="https://placeimg.com/192/192/people" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="h-32 bg-red-500"></div>
+        <div class="h-32 bg-red-500"></div>
+      </div>
+    </div>
+  </div>
 </template>
