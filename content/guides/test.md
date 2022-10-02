cfcc0f12160ed514cd96199c5b31a9ae25d4d61c
Eric-Weng <weng.eric8@gmail.com>
2022-10-02 00:43:27 -0600

    feat: Added active table of contents sidebar. Added Hero Image and Title section.

--- content/guides/test.md
+++ content/guides/test.md
@@ -17,6 +17,8 @@ StackEdit stores your files in your browser, which means all your files are auto
 
 ## Create files and folders
 
+### test
+
 The file explorer is accessible using the button in left corner of the navigation bar. You can create a new file by clicking the **New file** button in the file explorer. You can also create folders by clicking the **New folder** button.
 
 ## Switch to another file
@@ -28,6 +30,8 @@ All your files and folders are presented as a tree in the file explorer. You can
 You can rename the current file by clicking the file name in the navigation bar or by clicking the **Rename** button in the file explorer.
 
 ## Delete a file
+
+### test 2
 
 You can delete the current file by clicking the **Remove** button in the file explorer. The file will be moved into the **Trash** folder and automatically deleted after 7 days of inactivity.
 
--- pages/guides/_slug.vue
+++ pages/guides/_slug.vue
@@ -1,24 +1,101 @@
-<template>
-    <article class="prose">
-        <a href="#" v-scroll-to="'#update-a-publication'">Jump to the part of the page with the “anchor-name” id </a>
-        <nuxt-content :document="page" />
-    </article>
-</template>
-
 <script>
 export default {
-    name: 'Test',
-    async asyncData({ $content, params, error }) {
+  async asyncData({ $content, params, error }) {
+    const page = await $content("guides", params.slug)
+      .fetch()
+      .catch((err) => {
+        error({ statusCode: 404, message: "Page not found" });
+      });
 
-        const page = await $content('guides', params.slug)
-            .fetch()
-            .catch(err => {
-            error({ statusCode: 404, message: "Page not found" });
-            });
+    return {
+      page,
+    };
+  },
+  data() {
+    return {
+      currentlyActiveToc: "",
+      observer: null,
+      observerOptions: {
+        root: this.$refs.nuxtContent,
+        threshold: 1.0,
+      },
+    };
+  },
+  mounted() {
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const id = entry.target.getAttribute("id");
+        if (entry.isIntersecting) {
+          this.currentlyActiveToc = id;
+        }
+      });
+    }, this.observerOptions);
 
-        return {
-            page
-        };
-    }
-}
+    // Track all sections that have an `id` applied
+    document
+      .querySelectorAll(".nuxt-content h2[id], .nuxt-content h3[id]")
+      .forEach((section) => {
+        this.observer.observe(section);
+      });
+  },
+  beforeDestroy() {
+    this.observer.disconnect();
+  },
+  methods: {
+    tableOfContentsHeadingClick(link) {
+      this.currentlyActiveToc = link.id;
+    },
+  },
+};
 </script>
+
+<template>
+  <div class="flex flex-col">
+    <div class="relative h-64">
+        <img :src="page.image" :alt="page.alt" class="absolute w-full h-full object-cover"/>
+        <div class="absolute w-full bottom-0 backdrop-blur-md"> 
+            <h1 class="font-bold ml-2 my-3 text-4xl">{{page.title}}</h1>
+        </div>
+    </div>
+    <div class="grid grid-cols-9 mt-5 gap-5">
+      <!-- Sticky Side Table of Contents-->
+      <aside class="col-span-2">
+        <div class="sticky top-16">
+          <h2 class="uppercase font-medium text-xl lg:mt-16 tracking-wider">
+            Table of contents
+          </h2>
+          <nav class="mt-4">
+            <ul>
+              <li
+                @click="tableOfContentsHeadingClick(link)"
+                :class="{ 'pl-4': link.depth === 3 }"
+                class="toc-list"
+                v-for="link of page.toc"
+                :key="link.id"
+              >
+                <a
+                  :class="{
+                    'text-red-500 hover:text-red-600':
+                      link.id === currentlyActiveToc,
+                    'text-black hover:gray-900': link.id !== currentlyActiveToc,
+                  }"
+                  role="button"
+                  class="transition-colors duration-75 text-base mb-2 block"
+                  href="#"
+                  v-scroll-to="`#${link.id}`"
+                >
+                  {{ link.text }}
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </aside>
+
+      <!-- Main Article -->
+      <article class="prose mt-16 col-span-7">
+        <nuxt-content :document="page" />
+      </article>
+    </div>
+  </div>
+</template>
